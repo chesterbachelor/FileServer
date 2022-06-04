1c676d999af943ec0a44fed2ecdf9d5b8e000dc8
PiotrChromniak <piotr.chromniak@gmail.com>
2022-06-04 19:06:47 +0200

    Add filepath choice for client side

--- src/Client.java
+++ src/Client.java
@@ -1,20 +1,28 @@
-import java.io.BufferedOutputStream;
-import java.io.FileOutputStream;
-import java.io.InputStream;
+import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String args[]) {
-
         try {
             Socket socket = new Socket("localhost", 4422);
-            BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream("C:/image.jpg"));
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter the Msg : ");
+            String msg = scanner.nextLine();
+
+            OutputStream outputStream = socket.getOutputStream();
+            DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+            dataOutputStream.writeUTF(msg);
+
+            BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream("C:\\image.jpg"));
             byte[] buffer = new byte[1024];
             int count;
             InputStream in = socket.getInputStream();
             while ((count = in.read(buffer)) >= 0) {
                 out.write(buffer,0,count);
             }
+            dataOutputStream.close();
             socket.close();
         }catch (Exception e){
             System.out.println(e);
